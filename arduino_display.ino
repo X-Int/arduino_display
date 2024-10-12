bbbd0c50f455f3a1de565765f121d76805424d21
zxrrry <1362425073@qq.com>
2024-10-12 11:16:06 +0800

    update code

--- arduino_display.ino
+++ arduino_display.ino
@@ -1,11 +1,14 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_GC9A01A.h"
 #include "image.h"
-//#include <CST816_TouchLib.h>
-//#include <CST816S.h>
+#include "Arduino_GigaDisplayTouch.h"
 #include <Wire.h>
-//#include "cst816t.h"          // capacitive touch
 #include <Scheduler.h>
 #include <Servo.h>
 
@@ -28,9 +31,10 @@
 #define MID 48
 #define SET 50
 #define RST 52
-//#define TFT1_CS 10  // Chip select
-//#define TFT1_DC 7  // Data/command
-//#define TFT1_BL 5  // Backlight contro
+#define TFT1_CS 31  // Chip select
+#define TFT1_DC 33  // Data/command
+#define TFT1_RES 35
+
 
 
 #define DELTA 10 // Step to move images
@@ -40,13 +44,10 @@
 // negotiable. "Soft" SPI (using any pins) is an option but performance is
 // reduced; it's rarely used, see header file for syntax if needed.
 
-//SPIClass SPI_5(11,12,13,10);
+
 Adafruit_GC9A01A tft_left(TFT_CS, TFT_DC);
-//Adafruit_GC9A01A tft_right(&SPI1,TFT1_CS, TFT1_DC);
-
-//TwoWire Wire2(CTP_SDA, CTP_SCL);
-//cst816t touch(Wire2, CTP_RST, CTP_INT);
-
+Adafruit_GC9A01A tft_right(TFT1_CS, TFT1_DC);
+Arduino_GigaDisplayTouch touchDetector;
 
 Servo servo1, servo2;
 
@@ -84,6 +85,7 @@ uint16_t imageBuffer[IMAGE_WIDTH * IMAGE_HEIGHT];
 //定义图片的初始坐标位置
 int image_x = 0;
 int image_y = 0;
+int detection;
 
 void setup() {
   Serial.begin(115200);
@@ -98,14 +100,19 @@ void setup() {
   digitalWrite(TFT_BL, HIGH);  // Backlight on
 #endif                         // end TFT_BL
 
-  //设定五向按键所接串口为输出、上拉电阻模式
-  for (int i = 40; i <= 52; i += 2) { pinMode(i, INPUT_PULLUP); }
+  //set CS CS1
+  pinMode(TFT_CS,OUTPUT);
+  pinMode(TFT1_CS,OUTPUT);
+  digitalWrite(TFT_CS, HIGH);
+  digitalWrite(TFT1_CS, HIGH);
+  SPI.begin();
 
-//#if defined(TFT1_BL)
-//  pinMode(TFT1_BL, OUTPUT);
-//  digitalWrite(TFT1_BL, HIGH);  // Backlight on
-//#endif                         // end TFT_BL  
-
+  pinMode(TFT1_RES,OUTPUT);
+  digitalWrite(TFT1_RES,LOW);
+  delay(10);
+  digitalWrite(TFT1_RES,HIGH);
+  
+ 
   //设定五向按键所接串口为输出、上拉电阻模式
   for(int i=40;i<=52;i+=2){pinMode(i,INPUT_PULLUP);}   
   servo1.attach(2);
@@ -117,32 +124,34 @@ void setup() {
 
 void loop(void) {
   tft_left.setRotation(0);
-  // tft_right.setRotation(3);
+  //tft_right.setRotation(0);
   //testText();
   //photoText();
   //Serial.println(numImages);
   remote_test();
 }
 
-void loop2() {
-//   #if defined(TOUCH)//处理触摸事件
-//    if(touch.available()){
-//        int touch_x ,touch_y;
-//        // 获取触摸位置
-//        touch_x = touch.x;
-//        touch_y = touch.y;
-//        image_x = touch_x - (IMAGE_WIDTH / 2);
-//        image_y = touch_y - (IMAGE_HEIGHT / 2);
-//
+void loop2() {  
+  #if defined(TOUCH)//处理触摸事件
+        int touch_x ,touch_y;// 获取触摸位置
+        uint8_t contacts;
+        GDTpoint_t points[5];
+        contacts = touchDetector.getTouchPoints(points);
+        if(contacts > 0)
+        {
+          touch_x = points[0].x;// record touchpoint
+          touch_y = points[0].y;
+        }
+        image_x = touch_x - (IMAGE_WIDTH / 2);
+        image_y = touch_y - (IMAGE_HEIGHT / 2);
 //        Serial.print("Touch X: ");
 //        Serial.print(touch_x);
 //        Serial.print(" Touch Y: ");
 //        Serial.println(touch_y);
-//        tft_left.fillScreen(GC9A01A_BLACK);
-//        tft_left.drawRGBBitmap(image_x, image_y, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);
-//        delay(100);
-//      }
-// #endif
+        tft_left.fillScreen(GC9A01A_BLACK);
+        tft_left.drawRGBBitmap(image_x, image_y, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);
+        delay(100);
+  #endif
 }
 
 //修改图片数组
@@ -156,13 +165,16 @@ uint16_t* change_uint16(const unsigned char image[]) {
 }
 
 unsigned long photoText() {
-  tft_left.fillScreen(GC9A01A_BLACK);
+  //tft_left.fillScreen(GC9A01A_BLACK);
   //tft_right.fillScreen(GC9A01A_BLACK);
   unsigned long start = micros();
   change_uint16(gImage_eyes1);
   //demo8.fillScreen(0x0000);//初始化画布为黑色背景
   canvas.fillScreen(0x0000);  //初始化画布为黑色背景
+  //spi_display(1);
+  //spi_display(2);
   tft_left.drawRGBBitmap(0, 0, imageBuffer, canvas.width(), canvas.height());
+  //tft_right.drawRGBBitmap(0, 0, imageBuffer, canvas.width(), canvas.height());
   return micros() - start;
 }
 
@@ -186,15 +198,14 @@ void remote_test() {
   }
     }
 
- 
-
 void change_image() {
   image_x = 0;
   image_y = 0;  //清空图片位置
-  tft_left.fillScreen(GC9A01A_BLACK);
+  //tft_left.fillScreen(GC9A01A_BLACK);
   currentImageIndex = (currentImageIndex + 1) % numImages;  //更新索引
   change_uint16(images[currentImageIndex]);
   tft_left.drawRGBBitmap(0, 0, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);  // 绘制图片
+  //tft_right.drawRGBBitmap(0, 0, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT); 
 }
 
 void move_image(int direction, const int delta) {
@@ -207,5 +218,21 @@ void move_image(int direction, const int delta) {
   }
   //tft_left.fillScreen(GC9A01A_BLACK);
   tft_left.drawRGBBitmap(image_x, image_y, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);
+  //tft_right.drawRGBBitmap(image_x, image_y, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);
   delay(100);
 }
+
+void spi_display(int detection){
+    if(detection==1){
+        digitalWrite(TFT_CS,LOW);
+        delay(10);
+        tft_left.drawRGBBitmap(0, 0, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT); 
+        digitalWrite(TFT_CS,HIGH);
+      }
+    else if(detection==2){
+        digitalWrite(TFT1_CS,LOW);
+        delay(10);
+        //tft_right.drawRGBBitmap(0, 0, imageBuffer, IMAGE_WIDTH, IMAGE_HEIGHT); 
+        digitalWrite(TFT1_CS,HIGH);
+      }
+  }
